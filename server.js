e0a9990166f2e9ef8edae9949ccd199f13b1fa03
Eduardo <eduardo.damasceno5@gmail.com>
2021-01-30 22:08:26 -0300

    Try diferent approach to launch

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/birthday-gift'));
+app.use(express.static(`${__dirname}/dist/birthday-gift`));
 
-app.get('/*', (_req, res) => res.sendFile('index.html', { root: 'dist/birthday-gift/' }));
+app.get('/*', (_req, res) => res.sendFile(path.join(`${__dirname}/dist/birthday-gift/index.html`)));
 
 app.listen(process.env.PORT || 8080, function () {
     console.log('###########################################################');
