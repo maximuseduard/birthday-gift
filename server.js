391e62ea9429f23af20163756c89b1c0427c5744
Eduardo <eduardo.damasceno5@gmail.com>
2021-01-30 22:27:45 -0300

    Set aot for deploy

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist/birthday-gift`));
 
-app.get('/*', (_req, res) => res.sendFile(path.join(`${__dirname}/dist/birthday-gift/index.html`)));
+// app.get('/*', (_req, res) => res.sendFile('./dist/birthday-gift/index.html'));
+app.get('/*', (_req, res) => res.sendFile('index.html', { root: 'dist/angular-heroku/' }));
 
 app.listen(process.env.PORT || 8080, function () {
     console.log('###########################################################');
